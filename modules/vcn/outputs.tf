c53634b6a0198462d52287623b62ae2f834a2d6d
kartikhegde <kartikshrikanthegde@gmail.com>
2019-04-26 13:48:18 -0500

    Initial commit

---
+++ modules/vcn/outputs.tf
@@ -0,0 +1,5 @@
+# Output variables from created vcn
+
+output "subnet1_ocid" {
+  value = "${oci_core_subnet.subnet_ad1.id}"
+}
---
+++ vars.tf
@@ -0,0 +1,28 @@
+# Variables Exported from env.sh
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "ssh_public_key" {}
+variable "instance_shape" {}
+variable "image_ocid" {}
+variable "ssh_authorized_private_key" {}
+
+# Uses Default Value
+
+variable "instance_count" {
+  default="1"
+}
+
+# Provide a name for the instance name
+
+variable "compute_display_name" {
+  default="test_instance"
+}
+
+variable "availability_domain" {
+  default="1"
+}
